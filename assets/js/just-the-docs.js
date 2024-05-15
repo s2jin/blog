6c7daa785b866d361d9e86fc7e1360316606724e
sujin <nous645@gmail.com>
2024-05-15 22:54:14 +0900

    한국어 검색을 위한 파일 추가 및 수정

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -103,6 +103,7 @@ function initSearch() {
       lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
 
       var index = lunr(function(){
+        this.use(lunr.multiLanguage('en','ko'));
         this.ref('id');
         this.field('title', { boost: 200 });
         this.field('content', { boost: 2 });
